e1d4cec8ab83c57dc47d64656b3b29bfa29c11b4
Hafsi <104534187+Hafsi07@users.noreply.github.com>
2025-01-27 22:43:09 +0100

    txt_parsing_1

--- parser.py
+++ parser.py
@@ -1,2 +1,42 @@
 import json
-print('ciuao bello')
+import os 
+
+wd=''
+
+with open("message_1.json", 'r') as f:
+    data=json.load(f)
+
+
+recipiants=data.get('participants',None)
+
+# print(recipiants[0]['name'])
+# exit()
+def get_messages(data):
+    
+    messages=[]
+
+    for i in data['messages']:
+        x=i.get('content',None)
+        if x:
+            if x.find('@')>=0:
+                x=x.replace('@everyone','').strip().replace('  ',' ')
+                x=x.replace('@Meta Ai','').strip().replace('  ',' ')
+                for rec in recipiants:
+                    x=x.replace(f"@{rec['name']}",'').strip().replace('  ',' ')
+
+            if x.endswith('to your message '): 
+                print('valid')
+                continue
+
+            if x != '': messages.append(x)
+    return messages
+
+res= get_messages(data)
+print(res)
+print(len(res))
+# print(data['messages'][1]['content'])
+
+# @ + recipienets names  exclustion  from emssages
+# links and special characters exclusion
+
+
