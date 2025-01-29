f0c795f2203743ef42607c24e8da3769f0bdf60a
Hafsi <104534187+Hafsi07@users.noreply.github.com>
2025-01-29 18:33:00 +0100

    txt_parser_4

--- parser.py
+++ parser.py
@@ -2,15 +2,17 @@ import json
 import os
 
 
-def get_messages(data,participants_to_exclude=[]):
+def get_messages(data,participants_to_exclude=[],participants_to_include=[]):
         
     recipiants=data.get('participants',None)
-    messages=[]
+    if participants_to_include==[]: participants_to_include=recipiants
+    participants_to_include=[i for i in participants_to_include if i not in participants_to_exclude]
 
+    messages=[]
     for i in data['messages']:
         sender=i.get('sender_name',None)
-        if sender in participants_to_exclude:
-            continue
+        if sender in participants_to_exclude or sender not in participants_to_include:continue
+            
         x=i.get('content',None)
         if x:
             if x.find('@')>=0:
@@ -39,16 +41,20 @@ def get_messages(data,participants_to_exclude=[]):
     return messages
 
 
-def file_loader(wd=os.getcwd()):
+def file_loader(wd=os.getcwd(),participants_to_remove=[],participants_to_include=[]):
     text=[]
     for filename in os.listdir(wd):
         fp = os.path.join(wd, filename)
         if os.path.isfile(fp) and fp.endswith('.json'):
             with open(fp,'r') as f :
                 data=json.load(f)
-            text=text+['------']+get_messages(data,['Participants to remove'])
+            text=text+['------']+get_messages(data,participants_to_remove,participants_to_include)
         else: continue
     return text
-cc=file_loader()
+cc=file_loader(participants_to_include=['Hafsi Youssef'])
 print(cc)
 print(len(cc))
+
+
+# add timestamp and conversation ID to the parsed data
+# create a java application to give options to users to choose who to exclude and who to include and conversations from when to where 
